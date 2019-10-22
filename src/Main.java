4c872ae738c71add66775027f33a2904426be67f
Kovacs Patrik <kvcspatrik@gmail.com>
2019-10-22 19:49:57 +0300

    fixing stuff again

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
-import gui.Controller;
+// kppe0433
 import labyrinth.EnchantedMaze;
-import labyrinth.EnchantedMazeFactory;
 import labyrinth.Maze;
 import labyrinth.MazeFactory;
 
 public class Main {
     public static void main(String[] args) {
-        new EnchantedMaze();
+        new Maze(new MazeFactory()); // simple maze
+//        new EnchantedMaze(); // enchanted maze
+        /*
+            Explanation:
+            Locked - locked door
+            K - key in that room
+            NUMBER - the id number of the room
+            The field is generated randomly, so sometimes there is no solution.
+         */
     }
 }
